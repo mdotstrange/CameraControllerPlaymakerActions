9829e753692ac13f5df180d3aba1cb431049cfdd
mdotstrange <mdotstrange@gmail.com>
2017-01-17 00:45:48 -0800

    Add files via upload

---
+++ GetCameraAnchorOffset.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using com.ootii.Cameras;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.ScriptControl)]
+    [Tooltip("Gets the camera anchor offset for the camera script. ")]
+    public class GetCameraAnchorOffset : FsmStateAction
+    {
+        [RequiredField]
+
+        [CheckForComponent(typeof(CameraController))]
+        [Tooltip("The owner")]
+        public FsmOwnerDefault gameObject;
+        public FsmFloat offset;
+        public FsmBool everyFrame;
+        CameraController cam;
+        public override void Reset()
+        {
+            gameObject = null;
+            everyFrame = true;
+
+
+        }
+
+        public override void OnEnter()
+        {
+            if (!everyFrame.Value)
+            {
+                DoSetAnchOffset();
+                Finish();
+            }
+
+        }
+
+        public override void OnUpdate()
+        {
+            if (everyFrame.Value)
+            {
+                DoSetAnchOffset();
+            }
+        }
+
+        void DoSetAnchOffset()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+
+                return;
+            }
+            if (cam == null)
+            {
+                cam = go.GetComponent<CameraController>();
+            }
+
+            offset.Value = cam._AnchorOffset.y;
+           
+
+
+
+
+
+        }
+
+    }
+}
---
+++ SetCameraAnchorOffset.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using com.ootii.Cameras;
+
+namespace HutongGames.PlayMaker.Actions
+{
+    [ActionCategory(ActionCategory.ScriptControl)]
+    [Tooltip("Sets the camera anchor offset for the camera script. ")]
+    public class SetCameraAnchorOffset: FsmStateAction
+    {
+        [RequiredField]
+
+        [CheckForComponent(typeof(CameraController))]
+        [Tooltip("The owner")]
+        public FsmOwnerDefault gameObject;
+        public FsmFloat offset;
+        public FsmBool everyFrame;
+        CameraController cam;
+        public override void Reset()
+        {
+            gameObject = null;
+            everyFrame = true;
+
+
+        }
+
+        public override void OnEnter()
+        {
+            if (!everyFrame.Value)
+            {
+                DoSetAnchOffset();
+                Finish();
+            }
+
+        }
+
+        public override void OnUpdate()
+        {
+            if (everyFrame.Value)
+            {
+                DoSetAnchOffset();
+            }
+        }
+
+        void DoSetAnchOffset()
+        {
+            var go = Fsm.GetOwnerDefaultTarget(gameObject);
+            if (go == null)
+            {
+               
+                return;
+            }
+            if(cam == null)
+            {
+                cam = go.GetComponent<CameraController>();
+            }
+
+
+            cam._AnchorOffset = new Vector3(0f, offset.Value, 0f);
+          
+
+          
+
+
+        }
+
+    }
+}
